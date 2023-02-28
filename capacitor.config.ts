a170043fb10344be0cf02e5c47d112f32a07e52d
Ajin <nikeaji22@gmail.com>
2023-02-28 13:02:17 +0530

    mobile

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'phygital-ui',
+  webDir: 'dist/phygital_ui',
+  bundledWebRuntime: false
+};
+
+export default config;
