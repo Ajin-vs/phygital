e7c84e1a8435064f6887f3546dfcb3d042189b90
Ajin <nikeaji22@gmail.com>
2023-03-16 14:59:15 +0530

    exit_application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { App } from '@capacitor/app';
+import { Toast } from '@capacitor/toast';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'phygital_ui';
+  backEvent =0;
+
+  constructor(private router : Router){
+ 
+    App.addListener('backButton', ()=>{
+      console.log("curetn url",this.router.url);
+      
+      if(this.router.url.includes('/home')){
+        if(this.backEvent == 0){
+          Toast.show({
+            text: 'Please click BACK again to exit!',
+            duration:'short'
+          });
+        }
+       
+        this.backEvent = this.backEvent +1;
+        setTimeout(() => {
+          this.backEvent = 0;
+        }, 2000);
+        if(this.backEvent == 2){
+          App.exitApp();
+        }
+      }
+      
+    
+      if(this.backEvent !== 2){
+        window.history.back();
+      }
+      
+    })
+
+  
+   
+  }
+  
 }
