f240296e39d2702e5b249c6e7102c2e07e76f5eb
Ajin <nikeaji22@gmail.com>
2023-09-02 21:39:09 +0530

    ip_changes

--- src/app/appservice.service.ts
+++ src/app/appservice.service.ts
@@ -9,7 +9,7 @@ export class AppserviceService {
   constructor(public httpClient: HttpClient) { }
   bio=false;
   netSpeed = new BehaviorSubject(0);
-  public customerDomain ='http://15.206.203.178:8080'
+  public customerDomain ='http://3.110.215.188:8080'
 
   register(mobileNumber:any,firstName:any,lastName:any,publicKey:any,status:any):Observable<any>{
     return this.httpClient.post(`${this.customerDomain}/api/mvp/customer/user`,{mobileNumber,lastName,firstName,publicKey,status})
